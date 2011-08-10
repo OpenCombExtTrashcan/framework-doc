ba614492edc846b783e658107bb4e0dcdf7dc38b
zq@alee2002.com <myname@server>
2011-08-10 11:29:50 +0800

    refact

--- classes/builder/start.php
+++ classes/builder/start.php
@@ -2,14 +2,14 @@
 namespace jc\doc\classes\builder ;
 
 use jc\fs\FileSystem;
-use jc\system\AppFactory;
+use jc\system\ApplicationFactory;
 use jc\fs\imp\LocalFileSystem;
 use jc\system\Application;
 use jc\ui\xhtml\UIFactory;
 use jc\fs\FSO ;
 
 include_once __DIR__.'/../../../framework/inc.entrance.php' ;
-$aApp = AppFactory::createFactory()->create(__DIR__) ;
+$aApp = ApplicationFactory::singleton()->create(__DIR__) ;
 
 
 $aApp->classLoader()->addPackage(__NAMESPACE__,null,'/') ;
